37bfcb7de8a6fa72ecd892493233538657ed6b8b
afirstenberg <github@addventure.com>
2025-12-19 18:49:48 -0500

    lesson 05 - demo. layout.

--- lesson-05-state-management/demo/agent.py
+++ lesson-05-state-management/demo/agent.py
@@ -1,14 +1,14 @@
 import os
 from google.adk.agents import Agent
-from .shipping import shipping_agent
-from .inquiry import inquiry_agent
+from .agents.shipping import shipping_agent
+from .agents.inquiry import inquiry_agent
 
 model = "gemini-2.5-flash"
 
 # Helper to read prompt
 def read_prompt(filename):
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(script_dir, filename)
+    file_path = os.path.join(script_dir, "prompts", filename)
     with open(file_path, "r") as f:
         return f.read()
 
