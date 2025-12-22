fdb4caf44cbcf194a0f76e27329584d136f65887
afirstenberg <github@addventure.com>
2025-12-21 21:46:25 -0500

    lesson 07 - demo. Adjust inquiry to use multiple agents

--- lesson-07-rag/demo/shipping/agents/inquiry.py
+++ lesson-07-rag/demo/shipping/agents/inquiry.py
@@ -17,13 +17,26 @@ print(f"Connecting to Toolbox at {toolbox_url}")
 db_client = ToolboxSyncClient(toolbox_url)
 
 get_order_tool = db_client.load_tool("get-order")
+get_order_agent = Agent(
+    name="get_order_agent",
+    description="Handles questions about the status of orders",
+    model=model,
+    instruction=read_prompt("inquiry-order-prompt.txt"),
+    tools=[get_order_tool],
+)
 
-inquiry_instruction = read_prompt("inquiry-prompt.txt")
+policy_search_agent = Agent(
+    name="datastore_search_agent",
+    description="Handles questions about corporate store policies, including shipping policies",
+    model=model,
+    instruction=read_prompt("inquiry-policy-prompt.txt"),
+    tools=[datastore_search_tool],
+)
 
 inquiry_agent = Agent(
     name="shipping_inquiry_agent",
     description="Handles questions about shipping policies and tracking.",
     model=model,
-    instruction=inquiry_instruction,
-    tools=[get_order_tool, datastore_search_tool],
+    instruction=read_prompt("inquiry-prompt.txt"),
+    sub_agents=[get_order_agent, policy_search_agent],
 )
